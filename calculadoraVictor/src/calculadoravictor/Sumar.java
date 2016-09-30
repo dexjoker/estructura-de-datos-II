caae890ca30aaf692f7d68e9d89e24f549cbfb8b
unknown <Laboratorio5Pc8@U5-08.UCATEC.local>
2016-09-30 19:47:45 -0400

    tarea

---
+++ calculadoraVictor/src/calculadoravictor/Sumar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoravictor;
+
+/**
+ *
+ * @author Laboratorio5Pc8
+ */
+public class Sumar {public double num1;
+    public double num2;
+    public double resultado;
+    public Sumar(){}
+    public Sumar(double numero1){
+        this.num1=numero1;
+         
+     }
+
+     public static double suma1N(double n){
+                if(n == 1)  //caso base
+              return 1;
+           else
+              return n + suma1N(n-1);
+    }
+    public Double obtenerSuma(){
+    return this.num1;
+    }
+    
+}
+         
