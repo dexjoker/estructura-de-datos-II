fe29c3e521730d3ebd22ad4249e097a5b1463104
dexjoker <victorhugoluna_1124@hotmail.com>
2016-09-30 14:31:10 -0400

    Add files via upload

---
+++ CalculadoraVictor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoravictor;
+
+/**
+ *
+ * @author JOKER
+ */
+public class CalculadoraVictor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new frm_principal().show();
+//         frm_principal micalculadora = new frm_principal();
+//        //indicamos que al abrirse, se ubicará en la mitad de la pantalla al pasarle
+//        //el parámetro null
+//        micalculadora.setLocationRelativeTo(null);
+//        //indicamos que se haga visible
+//        micalculadora.setVisible(true);
+    }
+    
+}
---
+++ Sumar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoravictor;
+
+/**
+ *
+ * @author JOKER
+ */
+public class Sumar {
+    public double num1;
+    public double num2;
+    public double resultado;
+    public Sumar(){}
+    public Sumar(double numero1){
+        this.num1=numero1;
+         
+     }
+
+     public static double suma1N(double n){
+                if(n == 1)  //caso base
+              return 1;
+           else
+              return n + suma1N(n-1);
+    }
+    public Double obtenerSuma(){
+    return this.num1;
+    }
+    
+}
+         
+    
+    
